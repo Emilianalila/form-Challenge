c768cba46388d1267525c19fd4ba866fd6d73178
Emilianalila <emilianalila@gmail.com>
2024-02-05 17:26:50 +0100

    adding form3-articlesNY

---
+++ form1-async/index.js
@@ -0,0 +1,144 @@
+//Sending form information to an test API that simulates a RESTful server, 
+//Then I make an HTTP request using fetch, getting a promise as a result. I use async/await to handle it asynchronously waiting for it to resolve or not.
+//I use the method addEventListener(), which allows my application to respond to specific user events example:"click a submit button" without having to reload the page.
+
+let myNameInput = document.querySelector('.js-name'); //using "querySelector"
+let myLastName = document.querySelector('.js-lastName');
+let mySubmitButton = document.querySelector('.js-submitButton');
+let myCheckbox = document.getElementById('myCheck');// using "getElementById"
+let myCommentInput = document.getElementById('comment-input')
+
+myCheckbox.disabled = true;
+mySubmitButton.disabled = true;
+
+function checkCheckBox (){
+  if(myNameInput.value.length > 0 && myLastName.value.length > 0){
+    myCheckbox.disabled = false;
+    mySubmitButton.disabled = false;
+  }else{
+    myCheckbox.disabled = true;
+    mySubmitButton.disabled = true;
+  } 
+}// utilizo esta manera porq aqui tengo q escuchar dos input que tienen las mismas validaciones, por eso es mas facil, crear una funcion sola y despues ejecutarlas en dos metodos diferentes sin repetir el codigo de la funcion dos veces.
+myNameInput.addEventListener('input', checkCheckBox); // if I add an eventListener to my variable, "I will follow every change of entry " and every time my input has a change when my page is refresh
+myLastName.addEventListener('input', checkCheckBox);
+checkCheckBox();
+
+let myInputEmail = document.querySelector('.js-inputEmail');
+myInputEmail.style.display = 'none'; // how to hide an element with js
+
+myCheckbox.addEventListener('change', function(){ //aqui tengo q chequear una sola casilla
+  if(myCheckbox.checked === true){ // how to check if a box is checked or not 
+    myInputEmail.style.display = 'block';
+    
+  }else if(myCheckbox.disabled === false){
+    myInputEmail.style.display = 'none';  
+  }
+});
+
+async function fetchData(){
+  let data;
+  if (myCheckbox.disabled === false && myCheckbox.checked === true){ // && myInputEmail.value.length > 0
+    data = {
+      firstName: myNameInput.value,
+      lastName: myLastName.value,
+      comment: myCommentInput.value,
+      isSubscribed: true,
+      email: myInputEmail.value
+    };
+  }else {
+    console.log("Checked the box and add your Email")
+  }
+  const options = {
+    method: 'POST', // Método HTTP POST
+    headers: {  
+      'Content-Type': 'application/json'                          // Tipo de contenido de la solicitud (en este caso, JSON)
+    },
+    body: JSON.stringify(data)                                   // Convertir los datos a formato JSON y enviarlos en el cuerpo de la solicitud
+  };
+  try{
+    const requestTypicode = await fetch ("https://jsonplaceholder.typicode.com/users", options);
+    if (!requestTypicode.ok) {
+      throw new Error("could not fetch resource");
+    }else{
+      const data = await requestTypicode.json(); // very importan, always remenber the await, this give me de data in format object
+      console.log(data);
+      console.log(data.firstName)
+      /* const displayData = document.createElement('div');
+      displayData.innerHTML = `Name: ${data.firstName}`;
+      document.body.append(displayData); */
+      let msjSuccessfulSubmission = document.createElement('p')
+      if (myCheckbox.checked === true){
+        msjSuccessfulSubmission.innerHTML = `Thanks for your subscribe "${data.firstName}".` //when client doesn't subscribe, don't show the Subccessful Submission message
+      }else {
+        msjSuccessfulSubmission.innerHTML = `Add your Email and checked the box for subscribe.`
+      }
+      document.body.append(msjSuccessfulSubmission);
+      setTimeout(()=>{
+         myNameInput.value = '';
+         myLastName.value = '';
+         myCommentInput.value = ''; 
+         myInputEmail.value = '';
+         myInputEmail.style.display = 'none';
+         myCheckbox.disabled = true;
+         mySubmitButton.disabled = true;
+        return msjSuccessfulSubmission.style.display = 'none';
+      },2000);
+    }
+  }catch(err){
+    console.error(err,'Oops something went wrong');
+  };
+}
+  
+
+  //JSONPlaceholder doesn't store data sent with POST requests, so you won't be able to recover that data with a subsequent GET request. If you need similar functionality in your application, consider using a real server or a suitable data storage service, such as "firebase".
+  // Si el campo de entrada está vacío cuando se envía el formulario, tu código personalizado dentro del controlador de eventos de envío mostrará un mensaje en la consola, pero el comportamiento predeterminado del navegador de enviar el formulario aún ocurrirá.(esto pasa cuando no utilizo el e.preventDefault())
+  
+  /* try{ ejercicio solicitud "Get" en jsonplaceholder
+    const getDataSent = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!getDataSent.ok) {
+      throw new Error('Error al obtener los datos');
+    }
+    const data2 = await getDataSent.json();
+    console.log(data2);
+    // Display the data in a new div on the page
+    const displayData2 = document.createElement('p');
+    displayData2.innerHTML = `Showing data received: ${data2[0].name}` ;
+    document.body.append(displayData2); // Nombre: ${data2.firstName}, Apellido: ${data2.lastName}, Email: ${data2.email}
+
+  }catch(err){
+    console.error(err, 'Oopsss')
+  }} */
+
+
+
+
+
+
+/* Exacto, estás utilizando una REST API (en este caso, jsonplaceholder.typicode.com) para realizar una solicitud GET y obtener datos. 
+Cuando realizas una solicitud HTTP utilizando fetch, obtienes una promesa como resultado. 
+Utilizas async/await para manejar de manera asíncrona esta promesa y 'esperar' a que se resuelva antes de continuar con el código. tambien se pueden utilizar promesas y sus metodos .then y catch para manejar los resultados (async/await es mas legible y facil de utilizar)
+El uso de async/await hace que tu código sea más legible y fácil de entender, ya que te permite escribir código asíncrono de manera 
+similar al código síncrono tradicional. En lugar de utilizar callbacks o promesas encadenadas, puedes usar await para esperar a que 
+una promesa se resuelva antes de continuar con la ejecución del código. Esto hace que tu código sea más claro y menos propenso a errores.
+En resumen, estás utilizando async/await para manejar la promesa devuelta por la solicitud HTTP y así poder trabajar de manera más cómoda con los datos obtenidos de la REST API. */
+
+
+
+
+
+/* let myCheckbox = document.getElementById('myCheck');
+let myNameInput = document.querySelector('.js-name');
+
+myCheckbox.disabled = true;
+
+myNameInput.addEventListener('input', function() {
+  const myName = myNameInput.value;
+  if(myName === ""){
+    console.log('Agrega un nombre');
+    myCheckbox.disabled = true;
+  } else {
+    console.log('Nombre ingresado:', myName);
+    myCheckbox.disabled = false;
+  }
+}); */
---
+++ from3-articlesNY/index.js
@@ -0,0 +1,36 @@
+async function fetchArticle() {
+  const apiKey = "EGA7ShrpG9u0bCNQRnSTuff9yWXLIGwZ";
+
+  try {
+    const inputSearch = document.getElementById("js-inputSearch").value.toLowerCase();
+    const requestNY = await fetch(
+      `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${inputSearch}&api-key=${apiKey}`
+    );
+    if (!requestNY.ok) {
+      throw new Error("could not fetch resource");
+    } else {
+      const datum = await requestNY.json();                            // If you don't use await with requestNY.json(), you'll get a promise instead of the resulting JavaScript object. Without await, you would be working with the promise itself rather than the resolved value of the promise.
+      console.log(datum.response.docs);                               // 'resolve: an array of 10'(10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+      console.log(datum.response.docs[0].multimedia[0].url);
+      
+
+      let container = document.querySelector(".js-container");
+      container.innerHTML = '';                                       //is used to clean up the contents of the container before adding new items after each search.
+
+      const title = document.createElement("h3");
+      title.innerHTML = datum.response.docs[0].lead_paragraph;
+      container.appendChild(title);
+
+      const body = document.createElement("p");
+      body.textContent = datum.response.docs[0].abstract;
+      container.appendChild(body);
+
+      const imgArticle = document.createElement("img");
+      imgArticle.src = `https://static01.nyt.com/${datum.response.docs[0].multimedia[0].url}`;
+      imgArticle.className = "css-img";
+      container.appendChild(imgArticle);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
